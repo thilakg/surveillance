631fa64abbc0884f0cc8b9eb016676da610f3cd2
thilak <thilakrex@gmail.com>
2014-06-20 18:01:43 +0530

    surveillance created:wq

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+
+	def index
+		# render "index"
+	end
+	def who
+		# render "who"
+		
+	end
+    def way
+		# render "way"
+		
+	end
+    def catch
+		# render "catch"
+		
+	end
+
+end
